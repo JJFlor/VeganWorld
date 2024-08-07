e69beab1855ef96ed76fe9400e146d6fd0a89e5c
jorgeyebenes <159541179+jorgeyebenes@users.noreply.github.com>
2024-08-07 09:14:49 +0000

    first commit

---
+++ src/front/js/pages/Crea_Usuario_Cliente.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Navbar } from "../component/navbar";
+
+import { Context } from "../store/appContext";
+
+export const Crea_Usuario_Cliente = () => {
+
+    const { store, actions } = useContext(Context);
+
+
+    return (
+
+    );
+};
