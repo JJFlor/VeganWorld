eda55f16f230a96bad4625c4f51ff34d35db41ce
Sebastian Sarria <161655160+JSebasSN@users.noreply.github.com>
2024-08-07 17:24:09 +0000

    avance landing

---
+++ src/front/js/component/FindBusiness.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const FindBusiness = () => {
+    return (
+        <div className="container-fluid">
+            <div className="mapSection">
+                <div className="map">aqui irá el mapa</div>
+            </div>
+            <div className="mapSection">
+                <a className="btn btnFind" href="">Find Your Vegan Spot 🌱</a>
+            </div>
+        </div>
+    )
+}
