39352760872567d85a72cbf752803504c4b8a952
Júlia Jaile Flores <73072636+JJFlor@users.noreply.github.com>
2024-08-17 09:13:04 +0000

    second commit

--- src/front/js/pages/Crea_Usuario_Cliente.js
+++ src/front/js/pages/Crea_Usuario_Cliente.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-import { Navbar } from "../component/navbar";
+import LogoAvocado from "../../img/logoAguacate.png";
 import { MdEdit } from "react-icons/md";
 import { Context } from "../store/appContext";
+import "../../styles/Crea_Usuario.css";
 
 export const Crea_Usuario_Cliente = () => {
 
@@ -17,7 +18,7 @@ export const Crea_Usuario_Cliente = () => {
                     <div className="d-flex flex-row align-items-center justify-content-center">
                         <div className="w-100">
                             <div>
-                                <img className="profile-img" src=""/>
+                                <img className="profile-img" src="" />
                             </div>
                             <div className="form-group mt-3">
                                 <p>Client Name</p>
@@ -25,38 +26,52 @@ export const Crea_Usuario_Cliente = () => {
                                     <MdEdit />
                                 </button>
                             </div>
-                           
-                                
-                                
-                        
-                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 className="modal-title fs-5" id="exampleModalLabel">What premium offers to your business?</h5>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <ul>
-                                                <li>Better positioning in our web.</li>
-                                                <li>Allows you to sell your products in our Shop.</li>
-                                                <li>Allows you to show your current offers.</li>
-                                            </ul>
-                                            <h6 className="only-for text-center mt-4">Only for 4,99€/month!</h6>
-                                        </div>
-                                        <div className="modal-footer">
-                                            <button type="button" className="btn btn-free" data-bs-dismiss="modal" onClick={() => { handlePartnerFreeClick() }}>Go Free</button>
-                                            <button type="button" className="btn btn-premium" data-bs-dismiss="modal" onClick={() => { handlePartnerPremiumClick() }}>Go Premium</button>
-                                        </div>
+                        </div>
+                        <div className="rollCards my-4">
+                            <div className="baseCard mx-3 mb-4">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <h5 className="card-title">Special title treatment</h5>
+                                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                        <a href="#" className="btn btnCards">Go somewhere</a>
                                     </div>
                                 </div>
                             </div>
-                            <div className="form-group mt-3">
-                                <input type="email" className="form-control" placeholder="Email address" onChange={(e) => setEmail(e.target.value)} value={email} />
+                            <div className="baseCard mx-3 mb-4">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <h5 className="card-title">Special title treatment</h5>
+                                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                        <a href="#" className="btn btnCards">Go somewhere</a>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="form-group mt-3">
-                                <input type={shown ? 'text' : 'password'} required={true} maxLength="12" minLength="6" className="form-control inputRelativeBusiness" placeholder="Password" onChange={(e) => { setPassword(e.target.value) }} value={password} />
-                                <span className="showPasswordBusiness" type="button" onClick={switchShown}>{shown ? <FaEye /> : <FaEyeSlash />}</span>
+                            <div className="baseCard mx-3 mb-4">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <h5 className="card-title">Special title treatment</h5>
+                                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                        <a href="#" className="btn btnCards">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="baseCard mx-3 mb-4">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <h5 className="card-title">Special title treatment</h5>
+                                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                        <a href="#" className="btn btnCards">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="baseCard mx-3 mb-4">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <h5 className="card-title">Special title treatment</h5>
+                                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                        <a href="#" className="btn btnCards">Go somewhere</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div>
@@ -65,7 +80,6 @@ export const Crea_Usuario_Cliente = () => {
                             </div>
                         </div>
                     </div>
-                    <button className="btn btn-signUp-user w-25 mt-4 shadow-lg" onClick={() => handleSignUp()}>Sign up</button>
                 </div>
             </div>
         </div>
