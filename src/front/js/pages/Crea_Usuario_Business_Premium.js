8357a8f31acfd97b17411951cc8ef31a4898125e
Júlia Jaile Flores <73072636+JJFlor@users.noreply.github.com>
2024-08-17 11:31:07 +0000

    eleventh commit

--- src/front/js/pages/Crea_Usuario_Business_Premium.js
+++ src/front/js/pages/Crea_Usuario_Business_Premium.js
@@ -30,45 +30,46 @@ export const Crea_Usuario_Business_Premium = () => {
                                 </div>
                             </div>
                         </div>
-                        <div>
-                            <h2 className="fav-places">Our business pictures</h2>
+                        <div className="current-offers w-75 text-center">
+                            <h2 className="mt-3">Current offers they have</h2>
                         </div>
-                        <div className="rollCards my-4 w-75">
-                            <div className="baseCard mx-3 mb-4 w-25">
-                                <div className="card">
-                                    <div className="card-body text-center">
-                                        <img className="business-pics" src={avatarImg} href="#" />
-                                        <p className="card-text">With supporting text below.</p>
-                                    </div>
+                        <div className="add-new-offers w-75">
+                            <h2 className="mt-3">Add new offer</h2>
+                            <div className="d-flex flex-row">
+                                <div>
+                                    <input type="text" className="form-control w-100" placeholder="Write what's the offer about" />
+                                </div>
+                                <div>
+                                    <button type="button" class="btn publish-btn">
+                                        Publish
+                                    </button>
                                 </div>
                             </div>
-                            <div className="baseCard mx-3 mb-4 w-25">
-                                <div className="card">
-                                    <div className="card-body text-center">
-                                        <img className="business-pics" src={avatarImg} href="#" />
-                                        <p className="card-text">With supporting text below.</p>
+                        </div>
+                        <div className="shop w-75">
+                            <h2>Shop</h2>
+                            <div className="d-flex flex-row">
+                                <div className=" mx-3 mb-4 w-25">
+                                    <div>
+                                        <div className="card-body shopProduct">
+                                            <img className="shop-pics" src={avatarImg} href="#" />
+                                            <p className="card-text text-start">Name Product, price in €</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="baseCard mx-3 mb-4 w-25">
-                                <div className="card">
-                                    <div className="card-body text-center">
-                                        <img className="business-pics" src={avatarImg} href="#" />
-                                        <p className="card-text">With supporting text below.</p>
+                                <div className=" mx-3 mb-4 w-25">
+                                    <div>
+                                        <div className="card-body shopProduct">
+                                            <img className="shop-pics" src={avatarImg} href="#" />
+                                            <p className="card-text text-start">Name Product, price in €</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="baseCard mx-3 mb-4 w-25">
-                                <div className="card">
-                                    <div className="card-body text-center">
-                                        <img className="business-pics" src={avatarImg} href="#" />
-                                        <p className="card-text">With supporting text below.</p>
-                                    </div>
+                                <div>
+                                    <button type="button" class="btn add-new-product-btn">
+                                        Add new product
+                                    </button>
                                 </div>
-                            </div>
-                            <div className="d-flex flex-row align-items-center justify-content-center mb-5">
-                                <button className="btn btnCards w-25 me-5">Contact us</button>
-                                <button className="btn btnCards w-25">Menu</button>
                             </div>
                         </div>
                     </div>
