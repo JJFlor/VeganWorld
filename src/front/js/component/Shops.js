b4b9bf2031c1b4778d011f78bb369b5f06780f3c
Sebastian Sarria <161655160+JSebasSN@users.noreply.github.com>
2024-08-26 08:21:01 +0000

    renderPartners

--- src/front/js/component/Shops.js
+++ src/front/js/component/Shops.js
@@ -1,89 +1,74 @@
-import React from "react";
-import BgImage from "../../img/fondoShop1.jpg"
+import React, { useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
+import BgImage from "../../img/fondoShop1.jpg";
 
 export const Shops = () => {
+    const { store, actions } = useContext(Context);
+
+    useEffect(() => {
+        actions.getAllPartnersInfo();  // Obtener la información de los socios al cargar el componente
+    }, []);
 
     const backgroundImage = {
         backgroundImage: `url(${BgImage})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
-    }
+    };
+
+    const handleCategoryClick = (typeOfServices) => {
+        if (typeOfServices === 'All') {
+            actions.setFilteredPartnerNull();  // Mostrar todos los socios
+        } else {
+            actions.setCathegoryFilter(typeOfServices);  // Filtrar socios por tipo de servicio
+        }
+    };
+
+    // Determinar qué socios mostrar: todos o filtrados
+    const partnersToDisplay = store.premiumPartnersFiltered || store.premiumPartners;
+
     return (
         <div className="text-center my-4">
             <div className="container">
                 <div className="row">
                     <div className="col my-2">
-                        <a className="btn btnCategory" href="">🍴 Restaurants</a>
+                        <button className="btn btnCategory" onClick={() => handleCategoryClick('Restaurant')}>🍴 Restaurants</button>
                     </div>
                     <div className="col my-2">
-                        <a className="btn btnCategory" href="">👜 Shops</a>
+                        <button className="btn btnCategory" onClick={() => handleCategoryClick('Shops')}>👜 Shops</button>
                     </div>
                     <div className="col my-2">
-                        <a className="btn btnCategory" href="">😄 Wellness</a>
+                        <button className="btn btnCategory" onClick={() => handleCategoryClick('Wellness')}>😄 Wellness</button>
                     </div>
                     <div className="col my-2">
-                        <a className="btn btnCategory" href="">🎮 Leisure</a>
+                        <button className="btn btnCategory" onClick={() => handleCategoryClick('Leisure')}>🎮 Leisure</button>
+                    </div>
+                    <div className="col my-2">
+                        <button className="btn btnCategory" onClick={() => handleCategoryClick('All')}>Show All</button>
                     </div>
                 </div>
 
                 <div className="rollCards my-4">
-                    <div className="baseCard mx-3 mb-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Special title treatment</h5>
-                                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" className="btn btnCards">Go somewhere</a>
+                    {partnersToDisplay && partnersToDisplay.map((partner, index) => (
+                        <div key={index} className="baseCard mx-3 mb-4">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">{partner.name}</h5>
+                                    <p className="card-text">{partner.type_of_services}</p>
+                                    <a href="#" className="btn btnCards">Go somewhere</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="baseCard mx-3 mb-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Special title treatment</h5>
-                                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" className="btn btnCards">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="baseCard mx-3 mb-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Special title treatment</h5>
-                                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" className="btn btnCards">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="baseCard mx-3 mb-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Special title treatment</h5>
-                                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" className="btn btnCards">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="baseCard mx-3 mb-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Special title treatment</h5>
-                                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" className="btn btnCards">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
             <div className="goShop" style={backgroundImage}>
                 <div className="container text-end">
-                    <a className="btn  btnShopNow">SHOP NOW!</a>
+                    <a className="btn btnShopNow">SHOP NOW!</a>
                 </div>
                 <div className="container">
                     <p className="txtDiscount text-start">Use code: 4GEEKS and get 20% discount</p>
                 </div>
-
             </div>
         </div>
-
-    )
+    );
 }
